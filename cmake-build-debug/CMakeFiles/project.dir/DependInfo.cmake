47a02e58ddace5c441185ad02a23c398dabcc392
Morteza Nosratpour <45389014+MortezaNosratpour@users.noreply.github.com>
2022-07-18 01:37:49 +0430

    First Commit

---
+++ cmake-build-debug/CMakeFiles/project.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/e/c/95107777/src/logic.c" "/cygdrive/e/c/95107777/cmake-build-debug/CMakeFiles/project.dir/src/logic.c.o"
+  "/cygdrive/e/c/95107777/src/physic.c" "/cygdrive/e/c/95107777/cmake-build-debug/CMakeFiles/project.dir/src/physic.c.o"
+  "/cygdrive/e/c/95107777/src/simple_snake.c" "/cygdrive/e/c/95107777/cmake-build-debug/CMakeFiles/project.dir/src/simple_snake.c.o"
+  "/cygdrive/e/c/95107777/src/ulter_tank.c" "/cygdrive/e/c/95107777/cmake-build-debug/CMakeFiles/project.dir/src/ulter_tank.c.o"
+  "/cygdrive/e/c/95107777/src/view.c" "/cygdrive/e/c/95107777/cmake-build-debug/CMakeFiles/project.dir/src/view.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../sdl2/sdl2-include"
+  "../sdl2/sdl2-gfx-include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/src/logic.c.o"
+  "CMakeFiles/project.dir/src/physic.c.o"
+  "CMakeFiles/project.dir/src/simple_snake.c.o"
+  "CMakeFiles/project.dir/src/ulter_tank.c.o"
+  "CMakeFiles/project.dir/src/view.c.o"
+  "project.pdb"
+  "project.exe"
+  "libproject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
